9a591af4dd9d368987c4bc1f30ab298a77ba5e41
coyubi <tora0neko30@gmail.com>
2024-11-27 15:31:36 +0900

    課題をコミット

---
+++ kadai_005/bmi.js
@@ -0,0 +1,12 @@
+
+
+let weight = 68;
+let height = 1.7;
+let bmi = weight/(height*height);
+
+console.log('体重：' + weight + 'kg')
+console.log('身長：' + height+ 'm')
+
+// console.log(bmi);
+
+console.log('BMI = ' + bmi)
