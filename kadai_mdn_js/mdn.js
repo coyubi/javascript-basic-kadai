00a4c6b4d94b49dfe5eab1de15d8c249e8dc1715
coyubi <tora0neko30@gmail.com>
2024-12-05 15:10:46 +0900

    課題MDNを追加

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+const executionDay = new Date();
+const date1 = executionDay.getFullYear();
+const date2 = executionDay.getMonth();
+const date3 = executionDay.getDate();
+
+
+console.log (date1 + '年' + date2 + '月' + date3 + '日');
